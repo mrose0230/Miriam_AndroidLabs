d4ef2d9b39aaff1ed170e296fdf8f7546f4c205f
miria <rose0230@algonquinlive.com>
2023-10-02 15:25:20 -0400

    Finished PHone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
@@ -3,12 +3,35 @@ package algonquin.cst2335.rose0230;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgview;
+    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgview=findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(50000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgview.startAnimation(rotate);
+            } else imgview.clearAnimation();
+
+        });
     }
+
 }
+
