014ae97a63cfd0c7fb4b4c064dd23dca8885ac8a
miria <rose0230@algonquinlive.com>
2023-10-06 09:24:31 -0400

    master with functions

--- app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
@@ -10,27 +10,36 @@ import android.widget.ImageView;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-ImageView imgview;
-Switch sw;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imgview=findViewById(R.id.flagview);
-        sw = findViewById(R.id.spin_switch);
-
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-            if (isChecked) {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(50000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgview.startAnimation(rotate);
-            } else imgview.clearAnimation();
-
-        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
