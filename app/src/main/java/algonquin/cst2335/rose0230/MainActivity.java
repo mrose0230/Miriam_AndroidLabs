6545a6f3099b949b3fb73640a2575bec04574f4c
miria <rose0230@algonquinlive.com>
2023-11-21 17:56:59 -0500

    week9 created in wrong place, ignore

--- app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+
     @Override
     protected void onStart() {
         super.onStart();
