28407cd35c9a2ea19bbc5856044e8737ca82ff0c
miria <rose0230@algonquinlive.com>
2023-11-27 12:32:48 -0500

    Sunset

---
+++ app/src/main/java/algonquin/cst2335/rose0230/ActivityMainBinding.java
@@ -0,0 +1,17 @@
+package algonquin.cst2335.rose0230;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.text.BreakIterator;
+
+public class ActivityMainBinding {
+
+    public static ActivityMainBinding inflate(LayoutInflater layoutInflater) {
+        return null;
+    }
+
+    public int getRoot() {
+        return 0;
+    }
+}
--- app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
@@ -1,47 +1,94 @@
 package algonquin.cst2335.rose0230;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
-import android.widget.Switch;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.rose0230.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue = null;
+    String latInput;
+    String longInput;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //This part goes at the top of the onCreate function:
+        queue = Volley.newRequestQueue(this);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+
+        binding.sendButton.setOnClickListener( click ->  {
+            String latitude = null;
+            String longitude = null;
+
+            String url = "api.sunrisesunset.io/json?lat="+ latitude +"&lng="+longitude+"&timezone=CA&date=today";
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    response -> {
+                        // this is called if it worked
+
+                        try {
+                            JSONObject results = response.getJSONObject("results") ;
+
+                            String sunrise = results.getString("sunrise");
+                            String sunset = results.getString("sunset");
+
+                            }
+                        catch (JSONException e){
+                            throw new RuntimeException(e);
+                        }
+
+
+
+                    });
+
+
+
+            queue.add(request); //this actually launches the request onResponse, or onErrorResponse will get called
+
+        });
+
     }
 
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 }
