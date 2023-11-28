e6e57a37f9f3a71c1cb818e641046d7712a9fd82
miria <rose0230@algonquinlive.com>
2023-11-27 19:17:07 -0500

    Sunset

--- app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rose0230/MainActivity.java
@@ -1,45 +1,132 @@
 package algonquin.cst2335.rose0230;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.Switch;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //This part goes at the top of the onCreate function:
+        queue = Volley.newRequestQueue(this);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+
+        binding.loginButton.setOnClickListener( click ->  {
+            String cityName = binding.passwordText.getText().toString();
+
+            //this goes in the button click handler:
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName) +
+                    "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    response -> {
+                        // this is called if it worked
+
+                        try {
+                            JSONObject main = response.getJSONObject("main");
+                            double temperature = main.getDouble("temp");
+
+                            double minimum = main.getDouble("temp_min");
+                            int humidity = main.getInt("humidity");
+
+
+                            JSONArray weather = response.getJSONArray("weather");
+                            int arraySize = weather.length();
+                            for(int i = 0; i < arraySize; i++)
+                            {
+                                JSONObject obj = weather.getJSONObject(i);
+                                String iconName = obj.getString("icon");
+
+                                String imageUrl = "http://openweathermap.org/img/w/" + iconName + ".png";
+
+                                File weatherImage = new File(getFilesDir() +"/" + iconName+  ".png");
+
+                                if(weatherImage.exists()){
+                                    Bitmap theImage = BitmapFactory.decodeFile(weatherImage.getAbsolutePath());
+                                    binding.weatherImage.setImageBitmap(theImage);
+                                }
+                                else {
+                                    ImageRequest imgReq = new ImageRequest(imageUrl,
+                                            (Response.Listener<Bitmap>) bitmap -> {
+                                                // Do something with loaded bitmap...
+                                                FileOutputStream fOut = null;
+                                                try {
+                                                    fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+
+                                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                                    fOut.flush();
+                                                    fOut.close();
+
+
+                                                } catch (FileNotFoundException e) {
+                                                    e.printStackTrace();
+
+                                                } catch (IOException e) {
+                                                    throw new RuntimeException(e);
+                                                }
+                                                binding.weatherImage.setImageBitmap(bitmap);
+
+
+                                            }, 1024, 1024, ImageView.ScaleType.CENTER, null,
+
+                                            (error) -> {
+                                                int i2 = 0;
+                                            });
+
+                                    queue.add(imgReq);
+                                }
+                            }
+
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+                    },
+                    error -> {
+                        //this is called if there is an error
+                        int i = 0;
+                    });
+
+
+
+            queue.add(request); //this actually launches the request onResponse, or onErrorResponse will get called
+
+        });
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 }
