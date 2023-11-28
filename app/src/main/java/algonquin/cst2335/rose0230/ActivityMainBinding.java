c441b114895c8f69b6f826e20549321ebe837c4a
miria <rose0230@algonquinlive.com>
2023-11-27 19:21:52 -0500

    Sunset

---
+++ app/src/main/java/algonquin/cst2335/rose0230/ActivityMainBinding.java
@@ -0,0 +1,22 @@
+package algonquin.cst2335.rose0230;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.xmlpull.v1.XmlPullParser;
+
+public class ActivityMainBinding {
+    public View loginbutton;
+    public XmlPullParser passwordText;
+    public ImageView weatherImage;
+    public View loginButton;
+
+    public static ActivityMainBinding inflate(LayoutInflater layoutInflater) {
+        return null;
+    }
+
+    public int getRoot() {
+        return 0;
+    }
+}
